bba424bf40629a8ad3283a246cfa7df485efd41b
杜张强 <x_mitsui@163.com>
2021-08-18 21:33:15 +0800

    TreeSet

---
+++ 基础语法/src/com/集合/TreeSetTest01.java
@@ -0,0 +1,29 @@
+package com.集合;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * TreeSet集合存储元素特点:
+ *  1. 无序不可重复, 但是存储的元素可以自动按照大小顺序排序, 称为: 可排序集合
+ *  2. 无序: 这里的无序指的是存进去的顺序和取出来的顺序不同, 并且没有下标
+ */
+public class TreeSetTest01 {
+    public static void main(String[] args) {
+        // 创建集合对象
+        Set<String> strs = new TreeSet<>();
+        // 添加元素
+        strs.add("B");
+        strs.add("C");
+        strs.add("C");
+        strs.add("G");
+        strs.add("F");
+        strs.add("E");
+        strs.add("M");
+        //遍历, 从小到大自动排序
+        for (String s :
+                strs) {
+            System.out.println(s);
+        }
+    }
+}
