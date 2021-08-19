a47ea374f77c7bc19e5178112787e65a8ea0143e
杜张强 <x_mitsui@163.com>
2021-08-19 22:14:58 +0800

    文件循环读取字节流

---
+++ 基础语法/src/com/IO流/FileInputStreamTest02.java
@@ -0,0 +1,49 @@
+package com.IO流;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 对第一个程序改进
+ */
+public class FileInputStreamTest02 {
+    public static void main(String[] args) {
+        // 创建文件字节输入流对象
+         FileInputStream fis = null;
+        try {
+        //   fis = new FileInputStream(System.getProperty("user.dir")+("\\FileInputStreamTest01"));
+            fis = new FileInputStream(System.getProperty("user.dir")+("/res/FileInputStreamTest01"));
+            // 读
+            // 方法返回值是读取到字节本身
+            /*
+            while(true){
+                int readData = fis.read();
+                if(readData==-1){
+                    break;
+                }
+                System.out.println(readData);
+            }
+            */
+            //改造while循环
+            int readData;
+            while((readData =fis.read())!= -1){
+                System.out.println(readData);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            // 关闭流的前提是: 流不是空, 流是空的时候没必要关闭
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
