bb4434e50d156baa4e9ac7061f7bd9313c77e25e
杜张强 <x_mitsui@163.com>
2021-08-22 21:17:40 +0800

    验证灵活性

---
+++ 多线程/Lesson/src/reflect/bean/User.java
@@ -0,0 +1,10 @@
+package reflect.bean;
+
+public class User {
+    public User(){
+        System.out.println("无参数构造方法");
+    }
+    public User(String s){
+        System.out.println("有参数构造方法");
+    }
+}
