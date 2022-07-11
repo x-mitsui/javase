a5c50fb5d404436462625df5c349d2deacdbc25a
dzq <x_mitsui@163.com>
2022-07-11 09:25:56 +0800

    Collection接口遍历

---
+++ 集合/src/CollectionIteratorTest.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * @Auther: x_mitsui
+ * @Date: 2022/7/11 - 07 - 11 - 09:16
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class CollectionIteratorTest {
+    public static void main(String[] args) {
+        Collection col1 = new ArrayList();
+        col1.add(1);
+        col1.add(2);
+        col1.add(3);
+        col1.add("str");
+        col1.add(false);
+
+        // 增强遍历
+        for (Object o:col1){
+            System.out.println(o);
+        }
+        System.out.println("------------");
+        // iterator
+        Iterator iterator = col1.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
